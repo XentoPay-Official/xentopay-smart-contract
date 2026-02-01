6f89fa9d7ceed135cef3d8c2bf8a43aa53d15875
xentopay <aceo.trade@gmail.com>
2026-02-01 15:59:25 +0400

    Create XentoPay.sol

---
+++ contracts/contracts/XentoPay.sol
@@ -0,0 +1,13 @@
+/**
+ * SPDX-License-Identifier: MIT
+ *
+ * XentoPay (XTPY) — BEP20 Smart Contract
+ * Official Contract Address:
+ * 0xfb2782baae94c5D999B9173F0559c5efeBF99d93
+ */
+
+pragma solidity ^0.8.0;
+
+contract XentoPay {
+    // Contract code will be added here
+}
